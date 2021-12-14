52006726a6f67e3e102a8b6baa2eb29111756bb9
niolha <olhanikulina@gmail.com>
2021-12-14 22:46:26 +0100

    Add file for check

---
+++ try_actions.py
@@ -0,0 +1,9 @@
+def squares(start, end):
+    squares = []
+    for value in range(start, end + 1):
+        square = value ** 2
+        squares.append(square)
+    return squares
+
+if "__main__" == __name__:
+    print(squares(1,11))
